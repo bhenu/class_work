bd8efd08d0486574d6d9e984a903b2529507c304
ghoshbinayak <ghoshbinayak@gmail.com>
2013-07-25 09:35:24 +0530

    calculator prigress

--- calculator/src/getop.c
+++ calculator/src/getop.c
@@ -2,13 +2,31 @@
 #include<ctype.h>
 #include "calc.h"
 
+int getop(char s[], int *j, char * exp);
+
+/*test*/
+int main(void)
+{
+    char exp[] = "1 2 + 56 - 98 54 *";
+    char op[20];
+    int j = 0;
+    while(exp[j] != '\0')
+    {
+        getop(op, &j, exp);
+        printf("%s", op);
+    }
+    return 0;
+}
 int getop(char s[], int *j, char * exp) /* output, index of current
                                        character in exp[], expression */
 {
     int i, c;
 
     while((s[0] = c = exp[(*j)]) == ' ' || c == '\t')
+    {
+        printf("%c", exp[(*j)]);
         (*j)++;
+    }
     s[1] = '\0';
     if (!isdigit(c) && c != '.')
         return c;       /*not a number*/
