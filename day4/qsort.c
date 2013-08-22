d10f0c3b843d71b64a95bab8f6c53d63271806e4
ghoshbinayak <ghoshbinayak@gmail.com>
2013-08-22 12:55:37 +0530

    qsort done but not working have to debug

--- day4/qsort.c
+++ day4/qsort.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define QSIZE 20
+#define MAXOP 100
 
 struct queue
 {
-    int elements[QSIZE]; //the array to hold the queue elements
-    int count; // number of elements present in the queue
-    int rear_pos; // index of the last element (should be initailised to -1)
-    int front_pos; // index of the first element of the queue (should be initialised to 0)
-}
-/* function prototypes */
+    int elements[QSIZE]; /* the array to hold the queue elements */
+    int count; /* number of elements present in the queue */
+    int rear_pos; /* index of the last element (should be initailised to -1) */
+    int front_pos; /* index of the first element of the queue (should be initialised to 0) */
+};
+
+/* function prototypes for queue */
 int insert(int value, struct queue * Queue);
 int delete(int * deleted, struct queue * Queue);
-void display(struct Queue);
+void display(struct queue);
 
+/* auxiliary functions */
+int fill_queue(struct queue *);
+int getop(char [], int *, char []);
+
+/* sort function */
+int Qsort(struct queue *, struct queue *);
+
+/* ######################### MAIN LOGIC ############################# */
 int main()
 {
+    struct queue Q1, Q2;
+    Q1.count = Q2.count = 0;
+    Q1.rear_pos = Q2.rear_pos = -1;
+    Q1.front_pos  = Q2.front_pos = 0;
+
+    /* take input into queue */
+    printf("Queue 1: \n");
+    fill_queue(&Q1);
+    printf("Queue 2: \n");
+    fill_queue(&Q2);
+
+    /* sorting algorithm */
+    Qsort(&Q1, &Q2);
+
+    display(Q2);
+    return 0;
 }
 
+/* ###############################O################################## */
+
+
 /* function to do the sorting */
-int qsort(struct queue * Q1, struct queue * Q2)
+int Qsort(struct queue * Q1, struct queue * Q2)
 {
     int deleted, i, j, smallest;
 
@@ -88,7 +120,7 @@ int delete(int *deleted, struct queue * Q)
 
 void display(struct queue Q)
 {
-    printf("[HEAD] ")
+    printf("[HEAD] ");
     while ( Q.count > 0)
     {
         printf("[%d] ", Q.elements[Q.front_pos]);
@@ -96,4 +128,55 @@ void display(struct queue Q)
         Q.count--;
     }
     printf("TAIL\n");
+}
+
+/* definition of getop function */
+int getop(char string[], int *idx, char op[])
+{
+    char c;     /* temp variable to store current char */
+    int i;
+
+    while ((op[0] = c = string[(*idx)++]) == ' ' || c == '\t')
+    /* loop through spaces */
+        ;
+    op[1] = '\0';
+
+    if (!isdigit(c) && c != '.') /* not a number or decimal point */
+        return 0;
+
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(op[++i] = c = string[(*idx)++]))
+            ;
+    if (c == '.')
+        while (isdigit(op[++i] = c = string[(*idx)++]))
+            ;
+    op[i] = '\0';
+
+    if(c != '\0')
+        (*idx)--;
+
+    return 1;
+}
+
+
+/* function to take input into the queue */
+int fill_queue(struct queue *myqueue)
+{
+    char input[MAXOP], op[20];
+    int type, indx = 0;
+
+    printf("Enter elements of the stack [type int] seperated by a space:\n");
+    fgets(input, MAXOP, stdin);
+
+    type = getop(input, &indx, op);
+    while(op[0] != '\0')
+    {
+        if (type)
+            insert(atoi(op), myqueue);
+
+        type = getop(input, &indx, op);
+    }
+
+    return 1;
 }
