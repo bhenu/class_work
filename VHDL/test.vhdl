1e9b44476d4f2bc1079e96301b0c7c0acd731776
Binayak Ghosh <ghoshbinayak@gmail.com>
2014-02-17 16:23:41 +0530

    mux test done

--- VHDL/test.vhdl
+++ VHDL/test.vhdl
@@ -8,44 +8,55 @@ end test;
 
 architecture behav of test is
 --  Declaration of the component that will be instantiated.
-component HalfAdder
-  port (a, b: in std_logic; sum, carry: out std_logic);
+component MUX1
+  port (d0, d1, d2, d3, s0, s1: in std_logic; X: out std_logic);
 end component;
 --  Specifies which entity is bound with the component.
-for HalfAdder_0: HalfAdder use entity work.HalfAdder;
-signal a, b, sum, carry: std_logic;
+for MUX1_0: MUX1 use entity work.MUX1;
+signal d0, d1, d2, d3, s0, s1, X:  std_logic;
 begin
 --  Component instantiation.
-HalfAdder_0: HalfAdder port map (a => a, b => b, sum => sum, carry => carry);
+MUX1_0: MUX1 port map (d0, d1, d2, d3, s0, s1, X);
 
 --  This process does the real job.
 process
    type pattern_type is record
-      --  The inputs of the HalfAdder.
-      a, b: std_logic;
-      --  The expected outputs of the HalfAdder.
-      sum, carry : std_logic;
+      --  The inputs of the MUX1.
+      d0: std_logic;      
+      d1: std_logic;
+      d2: std_logic;
+      d3: std_logic;      
+      s0: std_logic;
+      s1: std_logic;
+      --  The expected outputs of the MUX1.
+      X: std_logic;
    end record;
    --  The patterns to apply.
    type pattern_array is array (natural range <>) of pattern_type;
    constant patterns : pattern_array :=
-     (('0', '0', '0', '0'),
-      ('1', '0', '1', '0'),
-      ('0', '1', '1', '0'),
-      ('1', '1', '0', '1'));
+     (('0', '0', '0', '0', '0', '0', '0'),
+      ('0', '0', '0', '1', '0', '0', '1'),
+      ('0', '0', '0', '0', '0', '1', '0'),
+      ('0', '0', '1', '0', '0', '1', '1'),
+      ('0', '0', '0', '0', '1', '0', '0'),
+      ('0', '1', '0', '0', '1', '0', '1'),
+      ('0', '0', '0', '0', '1', '1', '0'),
+      ('1', '0', '0', '0', '1', '1', '1'));
 begin
    --  Check each pattern.
    for i in patterns'range loop
       --  Set the inputs.
-      a <= patterns(i).a;
-      b <= patterns(i).b;
+      d0 <= patterns(i).d0;      
+      d1 <= patterns(i).d1;          
+      d2 <= patterns(i).d2;          
+      d3 <= patterns(i).d3;          
+      s0 <= patterns(i).s0;          
+      s1 <= patterns(i).s1;
       --  Wait for the results.
-      wait for 1 ns;
+      wait for 20 ns;
       --  Check the outputs.
-      assert sum = patterns(i).sum
-         report "bad sum value" severity error;
-      assert carry = patterns(i).carry
-         report "bad carry value" severity error;
+      assert X = patterns(i).X
+         report "bad nand value" severity error;
     end loop;
    assert false report "end of test" severity note;
    --  Wait forever; this will finish the simulation.
