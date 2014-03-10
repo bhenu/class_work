cb3134323c31bde722a78dc5359828b97020f05c
Binayak Ghosh <ghoshbinayak@gmail.com>
2014-03-10 17:12:47 +0530

    broken comparator

--- VHDL/comparator.vhdl
+++ VHDL/comparator.vhdl
@@ -2,23 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 --------------------------------------
--- AND gate with three inputs
+
 entity COMPARATOR is
-	port (a, b: in std_logic; c, d, e: out std_logic);
-end COMPARATOR;
+port(	a,b: in std_logic;
+	c, d, e: out std_logic
+);
+end COMPARATOR;  
+
+---------------------------------------
 
 architecture behave of COMPARATOR is
-begin
-	process(a, b)
-	begin
-		if ((a xor b) = '0') then
-			c <= '1';
-		else
-			if (not a xor b = '0') then
-				d <= '1';
-			else
-				e <= '1';
-			end if;
-		end if;
-	end process;
+begin  
+    c <= a xor b;
+    d <= a;
+    e <= not a;
 end behave;
