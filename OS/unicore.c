f180553b3da59a7592ef18d72474e93859eb7e76
ghoshbinayak <ghoshbinayak@gmail.com>
2014-08-01 00:00:52 +0530

    improved the code

--- OS/unicore.c
+++ OS/unicore.c
@@ -13,7 +13,7 @@ int main(){
 	cpu_set_t core;
 	CPU_ZERO(&core);
 	CPU_SET(0, &core);
-	sched_setaffinity(0, sizeof(cpu_set_t), &core);
+	sched_setaffinity(0, sizeof(cpu_set_t), &core);  
 	
 	// Initialise the files
 	FILE *f1;
@@ -26,15 +26,15 @@ int main(){
 	// Get the time for each process 
 	if(pid == 0) {
         f2 = fopen("data2.dat", "w");
-        for (i = 0; i < 1000000; i++) {
+        for (i = 0; i < 100000; i++) {
                 gettimeofday(&current_time, NULL);
                 fprintf(f2, "%f\t2\n", (double)current_time.tv_usec);
         }
         fclose(f2);
 	}
-	else {	    
+	else {  
         f1 = fopen("data.dat", "w");
-        for (i = 0; i < 1000000; i++) {
+        for (i = 0; i < 100000; i++) {
                 gettimeofday(&current_time, NULL);
                 fprintf(f1, "%f\t1\n", (double)current_time.tv_usec);
         }
