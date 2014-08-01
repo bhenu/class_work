e9f682ed3986b9d6c5b9ee5773d2b75390ffbdee
Binayak Ghosh <ghoshbinayak@gmail.com>
2014-08-01 14:05:08 +0530

    zombie exp1 done

---
+++ OS/day2/zombie.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	int i;
+	pid_t n1;
+	n1 = fork();
+	for(i = 0; i < 11; i++) {
+		if (n1 != 0) {
+			printf("%d forked into %d\n", getpid(), n1);			
+			n1 = fork();
+		}
+		else{
+			break;		
+		}
+	}
+
+	if(n1 != 0) {
+		getchar();
+	}
+	
+	return 0;
+}
