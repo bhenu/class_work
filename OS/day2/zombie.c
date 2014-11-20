26ed59dd04adec7ce0d0fa391c829caa22f5baf2
ghoshbinayak <ghoshbinayak@gmail.com>
2014-11-21 02:08:51 +0530

    bugfix

--- OS/day2/zombie.c
+++ OS/day2/zombie.c
@@ -5,11 +5,10 @@ int main()
 {
 	int i;
 	pid_t n1;
-	n1 = fork();
 	for(i = 0; i < 11; i++) {
+		n1 = fork();
 		if (n1 != 0) {
 			printf("Parent(%d): Yo Talahashi.. Here comes a zombie no. %d\n", getpid(), n1);			
-			n1 = fork();
 		}
 		else break;		
 	}
