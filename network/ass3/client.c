10cf76f3660c64f6967e68248b7771ce17c8d8cb
Binayak Ghosh <ghoshbinayak@gmail.com>
2015-04-17 21:28:49 +0530

    playing with proxy server

--- network/ass3/client.c
+++ network/ass3/client.c
@@ -23,6 +23,7 @@ long timevaldiff(struct timeval *starttime, struct timeval *finishtime)
 }
 
 int main(int argc, char const *argv[]) {
+  
   // Create the socket
   int mysoc = socket(AF_INET, SOCK_STREAM, 0);
   int client;
@@ -31,74 +32,72 @@ int main(int argc, char const *argv[]) {
   if (mysoc == -1) {
     handle_error("socket create");
   }
-  
-  // Bind with address
 
-  /* Man page exerpt..
-
-    struct sockaddr_in {
-      sa_family_t    sin_family; // address family: AF_INET
-      in_port_t      sin_port;   // port in network byte order 
-      struct in_addr sin_addr;   // internet address 
-    };
-
-    // Internet address.
-    struct in_addr {
-      uint32_t       s_addr;     // address in network byte order 
-    };
-
-  */
-
+  // specify address
   struct sockaddr_in my_addr, client_addr;
-/*  in_port_t myport = 15005;
-  struct in_addr myip;
-  inet_aton("127.0.0.1", &myip);
-  my_addr.sin_family = AF_INET;
-  my_addr.sin_port = myport;
-  my_addr.sin_addr = myip;
-*/
-  in_port_t clientport = 15000;
+  in_port_t clientport = htons(8080);
   struct in_addr clientip;
-  inet_aton("127.0.0.1", &clientip);
+  inet_aton("10.24.0.1", &clientip);
   client_addr.sin_family = AF_INET;
   client_addr.sin_port = clientport;
   client_addr.sin_addr = clientip;
 
-  // experiment
-  // printf("%u\n", htons(4));
-
-/*  if(bind(mysoc, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in)) == -1){
-  	handle_error("bind");
-  }
-*/
   // Connect to server
   if(connect(mysoc, (struct sockaddr *)&client_addr, sizeof(struct sockaddr)) == -1){
     handle_error("connect");
   }
 
-  // Send/receive data
-  // char buf[100] = "hello";
-  char buf[100];
-  printf("%s\n", "enter string to invert");
-  gets(buf);
+  char c;
+  char buf[1024];
+  printf("%s\n", "ask the server to do something!");
+
+  int i = 0;
+  while((c=getchar()) != EOF){
+    buf[i++] = c;
+  }
+  buf[i] = '\0';
+  printf("%s\n", buf);
+
   send(mysoc, buf, strlen(buf), 0);
-  gettimeofday(&current_time, NULL);
-  int n = 0, i = 0;
-  while((n=recv(mysoc, (void *)&buf, 100, 0 )) > 0){
-  gettimeofday(&recv_time, NULL);
+  int n = 0;
+  i = 0;
+  while((n=recv(mysoc, (void *)&buf, 1024, 0 )) > 0){
     while(i < n){
       putchar(buf[i++]);
     }
     if(buf[n-1] == '\0')
       break;
   }
- 
-  printf("%ld\n", timevaldiff(&current_time, &recv_time));
+  
+  printf("%s\n", "ask the server to do something!");
+
+  i = 0;
+  while((c=getchar()) != EOF){
+    buf[i++] = c;
+  }
+  buf[i] = '\0';
+  printf("%s\n", buf);
+
+  send(mysoc, buf, strlen(buf), 0);
+  n = 0;
+  i = 0;
+  while((n=recv(mysoc, (void *)&buf, 1024, 0 )) > 0){
+    while(i < n){
+      putchar(buf[i++]);
+    }
+    if(buf[n-1] == '\0')
+      break;
+  }
 
   shutdown(mysoc, SHUT_RDWR);
-//  shutdown(client, SHUT_RDWR);
-//  close(client);
   close(mysoc);
   printf("\n%s\n", "done!");
   return 0;
 }
+
+
+// GET http://www.google.com/ HTTP/1.1
+// Host:www.google.com
+
+// CONNECT www.google.com:443 HTTP/1.1
+// Host: www.google.com:443
